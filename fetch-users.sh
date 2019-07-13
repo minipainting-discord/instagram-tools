62c2a8d884e3de7ac706fdf273ffcc70a6d3c694
François Vaux <madx@yapok.org>
2019-07-13 11:04:37 +0200

    Use good old curl to fetch users

--- fetch-users.sh
+++ fetch-users.sh
@@ -8,5 +8,5 @@ fi
 SHEET_ID=1_SX1PynC9whLMLWKYD1u1IWhM_5lt3vde5N4xOV8nJc
 SHEET_URL="https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1!A2:D?key=${GOOGLE_SHEETS_API_KEY}" 
 
-http "$SHEET_URL" |
-  jq '.values | map({discordName: .[0], igUrl: .[3]}) | map(select(.igUrl != null))' > users.json
+curl -s "$SHEET_URL" |
+  jq '.values | map({discordName: .[0], igUrl: .[3]}) | map(select(.igUrl != null))'
